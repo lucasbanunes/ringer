481afd167f5ded18135e6401829a8d92fb633af7
lucasbanunes <lucas.nunes@lps.ufrj.br>
2023-02-25 16:34:20 -0300

    Minor changes to jupyter_singularity.sh

--- scripts/jupyter_singularity.sh
+++ scripts/jupyter_singularity.sh
@@ -6,5 +6,6 @@
 # $sbatch jupyter_singularity.sh <image-name> <setup-script> <jupyter-port>
 # $sbatch -o $HOME/logs/jupyter_singularity.log jupyter_singularity.sh root-cern_latest.sif setup_repos_kepler.sh 1234
 # $sbatch jupyter_singularity.sh root-cern_latest.sif setup_repos_kepler.sh 1234
+# $sbatch jupyter_singularity.sh root-cern_latest.sif setup_repos.sh 1234
 
 singularity exec $HOME/imgs/$1 bash launch_jupyter.sh $2 $3
